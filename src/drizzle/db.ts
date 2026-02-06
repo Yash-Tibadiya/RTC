e1c65c421441703a06dbbc16bfdd00d7b8ea0705
Yash-Tibadia <tibadiyayash@gmail.com>
2026-02-06 10:26:38 +0530

    feat(db): add drizzle orm with neon database support
    This commit sets up Drizzle ORM with Neon Serverless database integration, including database configuration and schema setup, database migration scripts, and environment variable support.

---
+++ src/drizzle/db.ts
@@ -0,0 +1,7 @@
+import * as schema from "./schema";
+import { config } from "@/config/config";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(config.databaseUrl!);
+export const db = drizzle({ client: sql, schema });
